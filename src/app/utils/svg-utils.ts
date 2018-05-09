8febb0442d4c78c7a83f9172c51efd8713c7426c
0415fh <francis_fh@163.com>
2018-05-09 22:53:09 +0800

    'fdsfds'

--- src/app/utils/svg-utils.ts
+++ src/app/utils/svg-utils.ts
@@ -2,7 +2,21 @@ import { MdIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 
 export const loadSvgResources = (ir: MdIconRegistry, ds: DomSanitizer) => {
-    ir.addSvgIcon('category', ds.bypassSecurityTrustResourceUrl('assets/svgIcon/Category.svg'));
+    const imgUrl = 'assets/img/';
+    const svgUrl = 'assets/svgIcon/';
+    const sidebarIcon = `${imgUrl}sidebar/`;
+    const daydir = `${imgUrl}/days/`;
+    const days = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+    ];
+    ir.addSvgIcon('category', ds.bypassSecurityTrustResourceUrl(`${svgUrl}category.svg`));
+    ir.addSvgIcon('month', ds.bypassSecurityTrustResourceUrl(`${sidebarIcon}month.svg`));
+    ir.addSvgIcon('project', ds.bypassSecurityTrustResourceUrl(`${sidebarIcon}project.svg`));
+    ir.addSvgIcon('projects', ds.bypassSecurityTrustResourceUrl(`${sidebarIcon}projects.svg`));
+    ir.addSvgIcon('week', ds.bypassSecurityTrustResourceUrl(`${sidebarIcon}week.svg`));
+    days.forEach(d => ir.addSvgIcon(`day${d}`, ds.bypassSecurityTrustResourceUrl(`${daydir}day${d}.svg`)));
 };
 
 
